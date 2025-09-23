fc5bcdb32e6816d08b8cd338236bd5cd0b44766d
kkanthar <keerthanankantharuban@gmail.com>
2025-09-23 16:30:41 -0400

    This is to make sure it is lab2

--- Polynomial.java
+++ Polynomial.java
@@ -1,59 +1,93 @@
 public class Polynomial {
-	public double [] polynomial;
+	public double [] coefficients;
+	public int [] exponents;
+	
 	
 	public Polynomial() {
-		polynomial = new double[1];
+		coefficients = new double[1];
+		exponenents = new int[1];
 	}
 	
 
-	public Polynomial(double[] input_Array){
+	public Polynomial(double[] coefficients, int[] exponents){
+		
+		if (coefficients.length != exponents.length) {
+			return null; 
+		}
 
-		polynomial = new double[input_Array.length];
+		coefficients = new double[coefficients.length];
 
-		for (int i = 0; i < input_Array.length; i++){
+		for (int i = 0; i < coefficients.length; i++){
 
-			this.polynomial[i] = input_Array[i];
+			this.coefficients[i] = coefficients[i];
+			this.exponents[i] = exponents[i];
 		} 
 	}
 	
 
+	
+	//This should now compare exponenents, if they are same then add coefficients together. 
 	public Polynomial add(Polynomial polynomial_input){
 
-		Polynomial addedPolynomial;
-		double length;
-
-		if (this.polynomial.length >= polynomial_input.polynomial.length){
-
-			addedPolynomial = new Polynomial(this.polynomial);
-			length = polynomial_input.polynomial.length;
-
-			for (int i = 0; i  < length; i++)
-			addedPolynomial.polynomial[i] += polynomial_input.polynomial[i];
-
-		} else {
-
-			addedPolynomial = new Polynomial(polynomial_input.polynomial);
-			length = this.polynomial.length;
-
-			for (int i = 0; i  < length; i++)
-			addedPolynomial.polynomial[i] += this.polynomial[i];
-
+		Polynomial addedPolynomial = new Polynomial(this.coefficients, this.exponents);
+		
+		double length_input = polynomial_input.coefficients.length;
+		double length_this = this.coefficients.length;
+		
+		for (double exponent:this.exponents) {
+			for (int j = 0; j < length_input; j++) {
+				if (exponent == polynomial_input.exponents[j]) {
+					addedPolynomial.coefficients += polynomial_input.coefficients[j]; 
+					
+				}
+			}
 		}
+		
+			
+
 		
 		return addedPolynomial;
 	}
+	
 	
 
 	public double evaluate(double num){
 		double result = 0;
 
-		for(int i = 0; i < polynomial.length; i++){
+		for(int i = 0; i < coefficients.length; i++){
 
-			result += this.polynomial[i]*(Math.pow(num, i));
+			result += this.coefficients[i]*(Math.pow(num, this.exponents[i]));
 		}
 
 		return result;
 	}
+	
+	public Polynomial multiply(Polynomial polynomial_input) {
+		//Create result polynomial to store results
+		Polynomial result_polynomial;
+		result_polynomial.coefficients = new double[this.coefficients.length];
+		result_polynomial.exponents = new int[this.coefficients.length];
+		
+		Polynomial temp_result_polynomial;
+		temp_result_polynomial.coefficients = new double[1];
+		temp_result_polynomial.exponents = new int[1];
+		
+		
+		int product = 0;
+		
+		for(int i = 0; i < this.coefficients.length; i++) {
+			for (int j = 0; j < polynomial_input.coefficients.length; j++) {
+				//Use add function to add the result of multiplying coefficients
+				//and exponents together to the polynomail result
+				temp_result_polynomial.coefficients[0] = this.coefficients[i] * polynomial_input.coefficients[j];
+				temp_result_polynomial.exponents[0] = this.exponents[i] + polynomial_input
+				result_polynomial = result_polynomial.add(temp_result_polynomial);
+			}
+		}
+		
+		
+	}
+
 
 
 	public boolean hasRoot(double num){
